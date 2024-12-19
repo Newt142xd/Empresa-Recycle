b77371a9687cc6451a3afead5e115c5a594df1b2
Newman Escobar <newmansayan@gmail.com>
2024-12-18 22:35:13 -0500

    Se ajusto el texto y boton de la ventana de ver informacion en el boton de whatsapp, se esta implementando el ajuste para que la pagina sea responsive y se pueda visualizar perfectamente en otros dispositivos como moviles y diferentes tamaños

--- src/Js/Inicio.js
+++ src/Js/Inicio.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  const arrows = document.querySelectorAll(".arrow");
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+
+  arrows.forEach((arrow) => {
+    arrow.addEventListener("click", function (event) {
+      event.preventDefault();
+      const submenu = this.parentElement.nextElementSibling;
+      submenu.classList.toggle("show");
+      this.classList.toggle("open");
+    });
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+      menu.classList.remove("show");
+      document.querySelectorAll(".menu ul").forEach((submenu) => {
+        submenu.classList.remove("show");
+      });
+      document.querySelectorAll(".arrow").forEach((arrow) => {
+        arrow.classList.remove("open");
+      });
+    }
+  });
+});
+
 // Alternar barra lateral
 function toggleChat() {
   const sidebar = document.getElementById("whatsappSidebar");
