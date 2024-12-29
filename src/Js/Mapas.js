91385c35d835ed14ca6a3dfb6ba06049ce3ba7df
Newman Escobar <newmansayan@gmail.com>
2024-12-28 21:47:18 -0500

    Se ajustaron las secciones de toneladas y material donde estas se adaptan a las difenetes pantallas o dispositivos

--- src/Js/Mapas.js
+++ src/Js/Mapas.js
@@ -1,3 +1,49 @@
+//Se muestra el menu dentro del boton hamburguesa
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+
+  arrows.forEach((arrow) => {
+    arrow.addEventListener("click", function (event) {
+      event.preventDefault();
+      const submenu = this.parentElement.nextElementSibling;
+      submenu.classList.toggle("show");
+      this.classList.toggle("open");
+    });
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+      menu.classList.remove("show");
+      document.querySelectorAll(".menu ul").forEach((submenu) => {
+        submenu.classList.remove("show");
+      });
+      document.querySelectorAll(".arrow").forEach((arrow) => {
+        arrow.classList.remove("open");
+      });
+    }
+  });
+});
+
+//Funcion para mostrar la informacion de los mapas
+
+function showInfor(infoId) {
+  // Oculta todos los divs de información
+  const infoDivs = document.querySelectorAll(".info");
+  infoDivs.forEach((div) => {
+    div.classList.remove("active");
+  });
+
+  // Muestra solo el div seleccionado
+  const selectedDiv = document.getElementById(infoId);
+  if (selectedDiv) {
+    selectedDiv.classList.add("active");
+  }
+}
+
 // Alternar barra lateral
 function toggleChat() {
   const sidebar = document.getElementById("whatsappSidebar");
